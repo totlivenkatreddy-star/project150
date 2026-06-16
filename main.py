a8047ad92dfcc6f16e19aef0a83c4da50fb0b574
T VENKAT REDDY <venkatreddy8bp@gmail.com>
2026-06-17 03:15:07 +0530

    login feature

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Working"}
+# -------------------------
+# 1. LOGIN PAGE (UI)
+# -------------------------
+@app.get("/", response_class=HTMLResponse)
+def login_page():
+    return """
+    <h2>Login Page</h2>
+
+    <form action="/login" method="post">
+        <label>Username:</label><br>
+        <input type="text" name="username"><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password"><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+    """
