2e9385681451dfcd62a96e6bf4b834a88e72687c
T VENKAT REDDY <venkatreddy8bp@gmail.com>
2026-06-17 01:48:33 +0530

    added dictionary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
+from dict import employees
 print("🔥 Jenkins pipeline is working successfully!")
 print("Hello from VenkatReddy project 🚀")
 
 print("venkat reddy auto build test ")
+print("-------------------------------")
+for employee in employees:
+    print(employee,)
