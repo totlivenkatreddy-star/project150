ac1bf9448e4343a006b2aeb87c7ab54999ce988b
T VENKAT REDDY <venkatreddy8bp@gmail.com>
2026-06-22 02:13:32 +0530

    decorator practice

---
+++ decorator.py
@@ -0,0 +1,35 @@
+# # # def decorator(func):
+# # #     def additional_behaviour():
+# # #         print("user entered details")
+# # #         func()
+# # #         print("loggedout")
+# # #     return additional_behaviour    
+# # # @decorator
+# # # def login():
+# # #     print("user logged in successfully")
+
+# # # #login=decorator(login)
+# # # login()
+# # def decorator(func):
+# #     def wrapper():
+# #         print("card inserted in ATM")
+# #         func()
+# #         print("card removed in ATM")
+# #     return wrapper    
+
+
+# # def payment():
+# #     print("payment completed")
+# # payment=decorator(payment)  
+# # payment()
+# def decorator(func):
+#     def wrapper():
+#         print("database connection opened")
+#         func()
+#         print("database connection closed")
+#     return wrapper
+
+# def save_user():
+#     print("User saved")
+# save_user=decorator(save_user)
+# save_user()    
